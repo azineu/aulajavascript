d5b3e3e85b2da6c2d573a89359b3dbd17c3b319f
azineu <azineu.wng@2outlook.com>
2024-08-27 23:00:40 -0300

    aulas javascript

---
+++ AULA04/ex003.js
@@ -0,0 +1,5 @@
+var vel = 12
+console.log(`a velocidade do seu carro é ${vel}km/hr`)
+if(vel > 60){
+    console.log(`voce ultrapassou a velocidade`)
+}
---
+++ AULA04/ex004.js
@@ -0,0 +1,7 @@
+var pais = 'Brasil'
+console.log(`país de origem ${pais}`)
+if(pais=='Brasil'){
+    console.log("voce é brasileiro")
+}else{
+    console.log('voce é estrangeiro')
+}
